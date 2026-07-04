af29054602fbeba612d27113604d6d84c3800907
JKSoper-ctrl <jamessoper300@gmail.com>
2026-07-04 14:16:05 +0100

    feat: add input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::error::Error;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please input text to be translated into morse code:");
+    
+    let input: String = get_input().unwrap_or_else(|err| {
+        eprintln!("Error reading input: {err}");
+        process::exit(1);
+    });
+
+    println!("{input}");
+}
+
+fn get_input() -> Result<String, Box<dyn Error>> {
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string)?;
+    Ok(input_string.trim_end().to_string())
 }
