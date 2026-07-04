bfda5df640afb2f92e7bdb8bec2e255aaee1a45c
JKSoper-ctrl <jamessoper300@gmail.com>
2026-07-04 15:41:04 +0100

    feat: add text to morse functionality

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct UnsupportedCharError {pub unsupported_character: char}
+
+impl fmt::Display for UnsupportedCharError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "unsupported character: '{}'", self.unsupported_character)
+    }
+}
+
+impl std::error::Error for UnsupportedCharError {}
+
+pub fn string_to_morse(input: &str) -> Result<String, UnsupportedCharError> {
+    let mut morse = String::new();
+    
+    for character in input.chars() {
+        let morse_char = char_to_morse(character)?;
+        morse.push_str(morse_char);
+        morse.push(' ');
+    };
+
+    Ok(morse)
+}
+
+pub fn char_to_morse(character: char) -> Result<&'static str, UnsupportedCharError> {
+    match character {
+        'A' => Ok(".-"),
+        'B' => Ok("-..."),
+        'C' => Ok("-.-."),
+        'D' => Ok("-.."),
+        'E' => Ok("."),
+        'F' => Ok("..-."),
+        'G' => Ok("--."),
+        'H' => Ok("...."),
+        'I' => Ok(".."),
+        'J' => Ok(".---"),
+        'K' => Ok("-.-"),
+        'L' => Ok(".-.."),
+        'M' => Ok("--"),
+        'N' => Ok("-."),
+        'O' => Ok("---"),
+        'P' => Ok(".--."),
+        'Q' => Ok("--.-"),
+        'R' => Ok(".-."),
+        'S' => Ok("..."),
+        'T' => Ok("-"),
+        'U' => Ok("..-"),
+        'V' => Ok("...-"),
+        'W' => Ok(".--"),
+        'X' => Ok("-..-"),
+        'Y' => Ok("-.--"),
+        'Z' => Ok("--.."),
+        '0' => Ok("-----"),
+        '1' => Ok(".----"),
+        '2' => Ok("..---"),
+        '3' => Ok("...--"),
+        '4' => Ok("....-"),
+        '5' => Ok("....."),
+        '6' => Ok("-...."),
+        '7' => Ok("--..."),
+        '8' => Ok("---.."),
+        '9' => Ok("----."),
+        ' ' => Ok("/"),
+        other_character => Err(UnsupportedCharError{unsupported_character: other_character}),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::error::Error;
 use std::process;
+use morse_code_translator::string_to_morse;
 
 fn main() {
     println!("Please input text to be translated into morse code:");
@@ -10,7 +11,14 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{input}");
+    let uppercase_input = input.to_uppercase();
+    
+    let output: String = string_to_morse(&uppercase_input).unwrap_or_else(|err| {
+        eprintln!("Error translating text: {err}");
+        process::exit(1);
+    });
+
+    println!("{output}");
 }
 
 fn get_input() -> Result<String, Box<dyn Error>> {
